8e7473cfe376c898ac80e4fb412c692258ff3835
Pedro Bueno <pedro.ascenco@gmail.com>
2025-06-03 15:36:24 -0300

    Create Explicacao.java

---
+++ Explicacao.java
@@ -0,0 +1,17 @@
+public class Explicacao {
+    //Atributos:
+    private String Explicacao;
+
+    public Explicacao (String Explicacao) {
+        this.Explicacao = Explicacao;
+    }
+
+    public String getExplicacao() {
+        return Explicacao;
+    }
+
+    public void setExplicacao(String Explicacao) {
+        this.Explicacao = Explicacao;
+    }
+
+}
